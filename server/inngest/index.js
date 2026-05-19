73d53cc40a72538e0939c904ef6231b59301f240
S Ying Zung <syingzung205@gmail.com>
2026-05-18 23:20:40 -0300

    vercel error fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -100,7 +100,7 @@ const leaveApplicationReminder = inngest.createFunction(
 
 // Cron: Check attendance at 11:30 AM IST (06:00 UTC) and email absent employess
 const attendanceReminderCron = inngest.createFunction(
-  { id: "attendance-reminder-cron", triggers: [{cron: "0 0 6 * * *"}] },
+  { id: "attendance-reminder-cron", triggers: [{cron: "TZ=Asia/kolkata 30 11 * * *"}] },
    // 06:00 UTC = 11:30 AM IST
   async ({ step }) => {
     // Step 1: Get today's date range (IST)
